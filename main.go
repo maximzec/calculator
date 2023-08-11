eac2e43de1b1b98b01de1f212cced1d2a491b476
Maxim Frolenko <maximzec3200@gmail.com>
2023-08-12 01:51:29 +0300

    ByteStack structure finished

--- main.go
+++ main.go
@@ -2,7 +2,14 @@ package main
 
 import (
 	"fmt"
+	"github.com/calculator/stack"
 )
+
+func convertStrToRpn(s string) string {
+	for symbol := range s {
+
+	}
+}
 
 func main() {
 	fmt.Println("Hi!")
--- stack/stack.go
+++ stack/stack.go
@@ -1,4 +1,4 @@
-package main
+package stack
 
 //Структура стекового байта
 //В дальнейшем она потребуется для реализации перевода строки в польскую запись
@@ -6,14 +6,26 @@ package main
 
 type ByteStack []byte
 
+func createNewByteStack() ByteStack {
+	bs := make(ByteStack, 0)
+	return bs
+}
+
 func (bs ByteStack) empty() bool {
-	return true
+	return len(bs) == 0
 }
 
 func (bs ByteStack) push(b byte) bool {
+	bs = append(bs, b)
 	return true
 }
 
 func (bs ByteStack) pop() byte {
-	return 0
+	b := bs[len(bs)-1]
+	bs = bs[:len(bs)-1]
+	return b
+}
+
+func (bs ByteStack) peek() byte {
+	return bs[len(bs)-1]
 }
